5b1bae17b6984acea9daec8b8a154e90248d4211
Simon Ko <simonxeko@gmail.com>
2020-01-29 21:45:28 +0800

    Add comment repository

---
+++ src/CommentRepository.php
@@ -0,0 +1,168 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Simonxeko\PostComments;
+
+use Flarum\Discussion\Discussion;
+use Simonxeko\PostComments\Comment;
+use Flarum\User\User;
+use Illuminate\Database\Eloquent\Builder;
+
+class CommentRepository
+{
+    /**
+     * Get a new query builder for the comments table.
+     *
+     * @return Builder
+     */
+    public function query()
+    {
+        return Comment::query();
+    }
+
+    /**
+     * @param User|null $user
+     * @return Builder
+     */
+    protected function queryVisibleTo(User $user = null)
+    {
+        $query = $this->query();
+
+        if ($user !== null) {
+            // TODO: Fix visibility
+            //$query->whereVisibleTo($user);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Find a post by ID, optionally making sure it is visible to a certain
+     * user, or throw an exception.
+     *
+     * @param int $id
+     * @param \Flarum\User\User $actor
+     * @return \Flarum\Comment\Comment
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function findOrFail($id, User $actor = null)
+    {
+        // return $this->queryVisibleTo($actor)->findOrFail($id);
+        return $this->query()->findOrFail($id);
+    }
+
+    /**
+     * Find comments that match certain conditions, optionally making sure they
+     * are visible to a certain user, and/or using other criteria.
+     *
+     * @param array $where
+     * @param \Flarum\User\User|null $actor
+     * @param array $sort
+     * @param int $count
+     * @param int $start
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function findWhere(array $where = [], User $actor = null, $sort = [], $count = null, $start = 0)
+    {
+        /* $query = $this->queryVisibleTo($actor)
+            ->where($where)
+            ->skip($start)
+            ->take($count);*/
+        $query = $this->where($where)
+            ->skip($start)
+            ->take($count);
+
+        foreach ((array) $sort as $field => $order) {
+            $query->orderBy($field, $order);
+        }
+
+        return $query->get();
+    }
+
+    /**
+     * Find comments by their IDs, optionally making sure they are visible to a
+     * certain user.
+     *
+     * @param array $ids
+     * @param \Flarum\User\User|null $actor
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function findByIds(array $ids, User $actor = null)
+    {
+        $comments = $this->queryIds($ids, $actor)->get();
+
+        $comments = $comments->sort(function ($a, $b) use ($ids) {
+            $aPos = array_search($a->id, $ids);
+            $bPos = array_search($b->id, $ids);
+
+            if ($aPos === $bPos) {
+                return 0;
+            }
+
+            return $aPos < $bPos ? -1 : 1;
+        });
+
+        return $comments;
+    }
+
+    /**
+     * Filter a list of post IDs to only include comments that are visible to a
+     * certain user.
+     *
+     * @param array $ids
+     * @param User $actor
+     * @return array
+     */
+    public function filterVisibleIds(array $ids, User $actor)
+    {
+        return $this->queryIds($ids, $actor)->pluck('comments.id')->all();
+    }
+
+    /**
+     * Get the position within a discussion where a post with a certain number
+     * is. If the post with that number does not exist, the index of the
+     * closest post to it will be returned.
+     *
+     * @param int $discussionId
+     * @param int $number
+     * @param \Flarum\User\User|null $actor
+     * @return int
+     */
+    public function getIndexForNumber($discussionId, $number, User $actor = null)
+    {
+        $query = Discussion::find($discussionId)
+            ->comments()
+            ->whereVisibleTo($actor)
+            ->where('created_at', '<', function ($query) use ($discussionId, $number) {
+                $query->select('created_at')
+                      ->from('comments')
+                      ->where('discussion_id', $discussionId)
+                      ->whereNotNull('number')
+                      ->take(1)
+
+                      // We don't add $number as a binding because for some
+                      // reason doing so makes the bindings go out of order.
+                      ->orderByRaw('ABS(CAST(number AS SIGNED) - '.(int) $number.')');
+            });
+
+        return $query->count();
+    }
+
+    /**
+     * @param array $ids
+     * @param User|null $actor
+     * @return Builder
+     */
+    protected function queryIds(array $ids, User $actor = null)
+    {
+        // return $this->queryVisibleTo($actor)->whereIn('posts.id', $ids);
+        return $this->whereIn('comments.id', $ids);
+    }
+}
