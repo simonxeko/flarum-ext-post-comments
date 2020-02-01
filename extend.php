f899691b168ff3fa68cfa0a53c20a40a0e9db738
Simon Ko <simonxeko@gmail.com>
2020-02-01 22:16:27 +0800

    Add comment flag routes

--- extend.php
+++ extend.php
@@ -13,6 +13,13 @@ namespace Simonxeko\PostComments;
 use Illuminate\Events\Dispatcher;
 use Simonxeko\PostComments\CommentPolicy;
 
+use Simonxeko\PostComments\Controllers\CreateCommentFlagController;
+use Simonxeko\PostComments\Controllers\DeleteCommentFlagsController;
+use Simonxeko\PostComments\Controllers\ListCommentFlagsController;
+use Simonxeko\PostComments\Listeners\AddFlagsApiAttributes;
+use Simonxeko\PostComments\Listeners\AddFlagsApiDates;
+use Simonxeko\PostComments\Listeners\AddCommentFlagsRelationship;
+
 // use Flarum\Discussion\Event\Saving; ??
 use Flarum\Extend;
 
@@ -30,10 +37,16 @@ return [
         ->get('/comments/{id}','comments.show', Controllers\ShowCommentController::class)
         ->patch('/comments/{id}','comments.update', Controllers\UpdateCommentController::class)
         ->delete('/comments/{id}', 'comments.delete', Controllers\DeleteCommentController::class),
+    (new Extend\Routes('api'))
+        ->get('/comment_flags', 'comment_flags.index', ListCommentFlagsController::class)
+        ->post('/comment_flags', 'comment_flags.create', CreateCommentFlagController::class)
+        ->delete('/posts/{id}/comment_flags', 'comment_flags.delete', DeleteCommentFlagsController::class),
     new Extend\Compat(function (Dispatcher $events) {
         $events->subscribe(Listeners\AddPostCommentRelationship::class);
         $events->subscribe(Listeners\SaveCommentLikesToDatabase::class);
+        $events->listen(ConfigureModelDates::class, AddCommentFlagsApiDates::class);
+        $events->listen(Serializing::class, AddCommentFlagsApiAttributes::class);
+        $events->subscribe(AddCommentFlagsRelationship::class);
         $events->subscribe(CommentPolicy::class);
-        // $events->listen(Saving::class, Listeners\SaveCommentsToDatabase::class);
     }),
 ];
